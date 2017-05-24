fbe0a193eb1652b807b4da295a6c1eb7f82309bb
Bas Janssen <smh.janssen@gmail.com>
2017-05-24 12:58:22 +0200

    Fixed gui. Added labels to fields, added product field.

--- Codes/catkin_ws_multirob/src/multirob_test/src/gui.py
+++ Codes/catkin_ws_multirob/src/multirob_test/src/gui.py
@@ -16,17 +16,20 @@ pathLocations = "blah"
 
 class guiElements:
     def sendCommand(self, widget):
-        self.cmd_pub.publish(self.comboBoxSource.get_active_text(), self.comboBoxDestination.get_active_text(), "test", int(self.amount.get_text()))
-        print self.comboBoxSource.get_active_text()
-        print self.comboBoxDestination.get_active_text()
-        print self.amount.get_text()
+        if not rospy.is_shutdown():
+            self.cmd_pub.publish(self.comboBoxSource.get_active_text(), self.comboBoxDestination.get_active_text(), self.product.get_text(), int(self.amount.get_text()))
+        else:
+            print("Rospy is shutdown")
+        #print self.comboBoxSource.get_active_text()
+        #print self.comboBoxDestination.get_active_text()
+        #print self.amount.get_text()
+        #print int(self.amount.get_text())
         
     def destroy(self, widget, data=None):
-        gtk.main_quit()
+        sys.exit(0)#gtk.main_quit()
 
     def __init__(self):
-        self.cmd_pub = rospy.Publisher("pickup_command", cmdPickup, queue_size=10)
-        rospy.init_node('multirob_test_gui', anonymous=True)
+        self.cmd_pub = rospy.Publisher("pickup_command", cmdPickup, queue_size=0)
         
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("destroy", self.destroy)
@@ -51,18 +54,36 @@ class guiElements:
 
         self.amount = gtk.Entry(max=0)
 
+        self.product = gtk.Entry(max=0)
+        self.product.set_text("test")
+
         self.button = gtk.Button("Send")
         self.button.connect("clicked", self.sendCommand)
-
-        self.box = gtk.VBox(False, 0)        
+        
+        self.labelSource = gtk.Label("Source:")
+        self.labelDestination = gtk.Label("Destination:")
+        self.labelAmount = gtk.Label("Amount:")
+        self.labelProduct = gtk.Label("Product:")        
+        
+        self.box = gtk.VBox(False, 0)
+        self.box.add(self.labelSource)        
         self.box.add(self.comboBoxSource)
+        self.box.add(self.labelDestination)
         self.box.add(self.comboBoxDestination)
+        self.box.add(self.labelAmount)
         self.box.add(self.amount)
+        self.box.add(self.labelProduct)
+        self.box.add(self.product)
         self.box.add(self.button)
+        self.labelSource.show()
+        self.labelDestination.show()
+        self.labelAmount.show()
+        self.labelProduct.show()
         self.button.show()
         self.amount.show()
         self.comboBoxSource.show()
         self.comboBoxDestination.show()
+        self.product.show()
 
         self.window.add(self.box)
         self.box.show()
@@ -73,10 +94,14 @@ class guiElements:
 
 #print __name__
 if __name__ == "__main__":
+    rospy.init_node('multirob_test_gui', anonymous=True)
     rospack = rospkg.RosPack()
 
     packagePath = rospack.get_path("multirob_test")
     pathLocations = packagePath + "/locations/locations.txt"
         
     gui = guiElements()
-    gui.main()
+    #gui.main()
+    while (gtk.main_iteration(block=False)):
+        rospy.sleep(-1)
+        #rospy.spinOnce()
