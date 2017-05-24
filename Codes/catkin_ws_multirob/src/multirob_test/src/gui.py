f4269f7cec76893a580a96afbfe77d2c70339b1e
Bas Janssen <smh.janssen@gmail.com>
2017-05-24 10:55:43 +0200

    Added a simple gui to send commands, written in python.

---
+++ Codes/catkin_ws_multirob/src/multirob_test/src/gui.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+#example window creation with pygtk
+
+import sys
+import rospy
+import rospkg
+
+from multirob_test.msg import cmdPickup
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+pathLocations = "blah"
+
+class guiElements:
+    def sendCommand(self, widget):
+        self.cmd_pub.publish(self.comboBoxSource.get_active_text(), self.comboBoxDestination.get_active_text(), "test", int(self.amount.get_text()))
+        print self.comboBoxSource.get_active_text()
+        print self.comboBoxDestination.get_active_text()
+        print self.amount.get_text()
+        
+    def destroy(self, widget, data=None):
+        gtk.main_quit()
+
+    def __init__(self):
+        self.cmd_pub = rospy.Publisher("pickup_command", cmdPickup, queue_size=10)
+        rospy.init_node('multirob_test_gui', anonymous=True)
+        
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("destroy", self.destroy)
+        self.window.set_border_width(10)
+                
+        self.comboBoxSource = gtk.combo_box_new_text()
+        self.comboBoxSource.append_text("Select Source")
+        self.comboBoxDestination = gtk.combo_box_new_text()
+        self.comboBoxDestination.append_text("Select Destination")
+        
+        with open(pathLocations) as f:
+            for line in f:
+                #print line
+                line.split('\t')
+                self.comboBoxSource.append_text(line[0])
+                self.comboBoxDestination.append_text(line[0])
+                if 'str' in line:
+                    break
+                
+        self.comboBoxSource.set_active(0)
+        self.comboBoxDestination.set_active(0)
+
+        self.amount = gtk.Entry(max=0)
+
+        self.button = gtk.Button("Send")
+        self.button.connect("clicked", self.sendCommand)
+
+        self.box = gtk.VBox(False, 0)        
+        self.box.add(self.comboBoxSource)
+        self.box.add(self.comboBoxDestination)
+        self.box.add(self.amount)
+        self.box.add(self.button)
+        self.button.show()
+        self.amount.show()
+        self.comboBoxSource.show()
+        self.comboBoxDestination.show()
+
+        self.window.add(self.box)
+        self.box.show()
+        self.window.show()
+        
+    def main(self):
+        gtk.main()
+
+#print __name__
+if __name__ == "__main__":
+    rospack = rospkg.RosPack()
+
+    packagePath = rospack.get_path("multirob_test")
+    pathLocations = packagePath + "/locations/locations.txt"
+        
+    gui = guiElements()
+    gui.main()
